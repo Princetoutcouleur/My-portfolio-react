a919a07bf232a9c4cbd215a1de54109f9ac76da5
Princetoutcouleur <babathiam0000@gmail.com>
2023-11-12 13:39:12 +0000

    remplacer les icones par des images

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,33 +1,25 @@
-import React from 'react'
+import React from "react";
 import "./contact.css";
-import { FaEnvelope, } from "react-icons/fa";
+import Mail from "../../assets/icons/mail.png";
+import whatsApp from "../../assets/icons/WhatsApp.webp";
+import instagram from "../../assets/icons/instagram.png";
+import twitter from "../../assets/icons/twitter-x.png";
 import { FaLinkedin } from "react-icons/fa";
-import { BsWhatsapp } from "react-icons/bs";
-import { FaInstagram } from "react-icons/fa";
-
-
 
 const Contact = () => {
   function handleWhatsAppClick() {
-    const defaultText = encodeURIComponent(
-      "Salut Baba Thiam."
-    );
+    const defaultText = encodeURIComponent("Salut Baba Thiam.");
     const phoneNumber = "771010502";
 
-    window.open(
-      `https://wa.me/${phoneNumber}?text=${defaultText}`,
-      "_blank"
-    );
+    window.open(`https://wa.me/${phoneNumber}?text=${defaultText}`, "_blank");
   }
   return (
     <div id="contact" className="min-vh-100 pt-5">
       <div className="container text-light pt-5">
         <div className="row text-center">
           <h4 className="text-danger">Get in touch 😉</h4>
-          <h1 className='fw-bold'>Contact me</h1>
-          <li className="mb-3 online">
-            I'm currently available for work.
-          </li>
+          <h1 className="fw-bold">Contact me</h1>
+          <li className="mb-3 online">I'm currently available for work.</li>
           <p>
             I'm always looking for new opportunities to help you build your
             product.
@@ -35,26 +27,48 @@ const Contact = () => {
           <p>Feel free to contact me. I'm waiting for your message.</p>
 
           <div>
-            <ul className='list-unstyled d-flex gap-5 justify-content-center mt-5'>
-              <li className='email'>
-                <a href="mailto:babathia0000@gmail.com"
-              className="text-decoration-none email"><FaEnvelope size={30} /></a>
+            <ul className="list-unstyled d-flex gap-5 justify-content-center mt-5">
+              <li className="email">
+                <a
+                  href="mailto:babathia0000@gmail.com"
+                  className="text-decoration-none mail"
+                >
+                  <img src={Mail} alt="" />
+                </a>
               </li>
-              <li className='linkedin'>
-                <a href="https://www.linkedin.com/in/amadou-mamadou-thiam-a7b86222a/"
-              className="text-decoration-none linkedin"><FaLinkedin size={30} /></a>
+              <li className="linkedin">
+                <a
+                  href="https://www.linkedin.com/in/amadou-mamadou-thiam-a7b86222a/"
+                  className="text-decoration-none linkedin"
+                >
+                  <FaLinkedin className="icon" />
+                </a>
               </li>
-              <li className='whatsapp'>
-              <a id="whatsApp" href="#" onClick={handleWhatsAppClick} className="text-decoration-none whatsapp">
-              <BsWhatsapp size={30} />
-  </a>
- 
+              <li className="whatsapp">
+                <a
+                  id="whatsApp"
+                  href="#"
+                  onClick={handleWhatsAppClick}
+                  className="text-decoration-none icon"
+                >
+                  <img src={whatsApp} alt="" />
+                </a>
               </li>
-              <li className='instagram'>
-                <a href="https://www.instagram.com/the_princetoutcouleur/"
-              className="text-decoration-none text-light instagram">
-                <FaInstagram size={30} />
-              </a>
+              <li className="instagram">
+                <a
+                  href="https://www.instagram.com/the_princetoutcouleur/"
+                  className="text-decoration-none text-light icon"
+                >
+                  <img src={instagram} alt="" />
+                </a>
+              </li>
+              <li className="twitter">
+                <a
+                  href="https://twitter.com/Babathiam01"
+                  className="text-decoration-none text-black icon"
+                >
+                  <img src={twitter} alt="" />{" "}
+                </a>
               </li>
             </ul>
           </div>
@@ -62,6 +76,6 @@ const Contact = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
